b11691c9acca9a123b2a57f565b34b641d7e3e3d
saksham-accio <125554544+saksham-accio@users.noreply.github.com>
2023-05-21 18:24:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-//your code here
 function Person(name, age) {
-  this.name = name;
-  this.age = age;
+    this.name = name;
+    this.age = age;
 }
 
-Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
-  Person.call(this, name, age);
-  this.jobTitle = jobTitle;
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
 }
 
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-Employee.prototype.jobGreet = function () {
-  console.log(
-    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
-  );
+
+Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
-window.Person = Person;
-window.Employee = Employee;
+
+const person = new Person('John', 30);
+person.greet(); // Output: Hello, my name is John, I am 30 years old.
+
+const employee = new Employee('Jane', 25, 'Manager');
+employee.greet(); // Output: Hello, my name is Jane, I am 25 years old.
+employee.jobGreet(); // Output: Hello, my name is Jane, I am 25 years old, and my job title is Manager.
